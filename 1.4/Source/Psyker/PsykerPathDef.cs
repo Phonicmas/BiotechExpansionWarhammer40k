df20ac788429abad2b22a0ccd2a66daf3319d0a8
Phonicmas <jkskovlund@gmail.com>
2022-10-25 18:55:26 +0200

    More Stuff

---
+++ 1.4/Source/Psyker/PsykerPathDef.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using VanillaPsycastsExpanded;
+
+namespace Psyker
+{
+    public class PsykerPathDef : PsycasterPathDef
+    {
+        private GeneDef requiredGeneAny;
+
+        public override bool CanPawnUnlock(Pawn pawn) => PawnHasGene(pawn);
+
+        //private bool PawnHasGene (Pawn pawn) => true;
+        private bool PawnHasGene(Pawn pawn) => requiredGeneAny == null || pawn.genes.HasXenogene(requiredGeneAny);
+    }
+}
+
